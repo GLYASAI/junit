6c4b4af06ab26dce85085b7ac352a593c06083f9
GLYASAI <chaoschunqfeijg@163.com>
2018-05-17 23:15:15 +0800

    初始化maven项目，并初步是用junit

---
+++ src/main/java/org.abewang.junit/chapter1/Calculator.java
@@ -0,0 +1,13 @@
+package org.abewang.junit.chapter1;
+
+/**
+ * 用于测试的Calculator类
+ *
+ * @Author Abe
+ * @Date 2018/5/17.
+ */
+public class Calculator {
+    public double add(double number1, double number2) {
+        return number1 + number2;
+    }
+}
---
+++ src/test/java/org.abewang.junit/chapter1/CalculatorTest.java
@@ -0,0 +1,19 @@
+package org.abewang.junit.chapter1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 使用JUnit编写CalculatorTest程序
+ *
+ * @Author Abe
+ * @Date 2018/5/17.
+ */
+public class CalculatorTest {  // 这个类必须是公有的，通常在类后面加Test
+    @Test
+    public void testAdd() {  // 最好用testXXX()的方式命名
+        Calculator cal = new Calculator();
+        assertEquals(60, cal.add(10, 50), 0);
+    }
+}
