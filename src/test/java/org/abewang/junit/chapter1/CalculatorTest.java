2130be44bb849af9ef29e25b3eb63124fd38d2d3
GLYASAI <chaoschunqfeijg@163.com>
2018-05-17 23:44:02 +0800

    修改CalculatorTest的包

--- src/test/java/org/abewang/junit/chapter1/CalculatorTest.java
+++ src/test/java/org/abewang/junit/chapter1/CalculatorTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class CalculatorTest {  // 这个类必须是公有的，通常在类后面加Test
     @Test
-    public void testAdd() {  // 最好用testXXX()的方式命名
+    public void testAdd() {  // 测试方法必须是公共的，最好用testXXX()的方式命名
         Calculator cal = new Calculator();
         assertEquals(60, cal.add(10, 50), 0);
     }
