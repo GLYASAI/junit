69e394eac11ed00a3d935a1e8688c1dbbf7f4e55
GLYASAI <670862760@qq.com>
2018-05-18 11:32:04 +0800

    重构，提取重复代码

--- src/test/java/org/abewang/junit/chapter3/TestDefaultController.java
+++ src/test/java/org/abewang/junit/chapter3/TestDefaultController.java
@@ -15,30 +15,26 @@ import static org.junit.Assert.assertSame;
  */
 public class TestDefaultController {
     private DefaultController controller;
+    private SampleRequest request;
+    SampleRequestHandler handler;
 
     @Before  // @Before/@After注释的方法会发生在每一个＠Test方法的之前／之后，并且不管测试是遥远还是失败。
-    public void instantiate() throws Exception {
+    public void initialize() throws Exception {
         controller = new DefaultController();
+        request = new SampleRequest();
+        handler = new SampleRequestHandler();
+
+        controller.addHandler(request, handler);
     }
 
     @Test
     public void testGetHandler() {
-        SampleRequest request = new SampleRequest();
-        SampleRequestHandler handler = new SampleRequestHandler();
-        try {
-            controller.addHandler(request, handler);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         assertSame("The handler we set in controller should be the same handler we get",
                 handler, controller.getHandler(request));
     }
 
     @Test
     public void testProcessRequest() {
-        SampleRequest request = new SampleRequest();
-        SampleRequestHandler handler = new SampleRequestHandler();
-        controller.addHandler(request, handler);
         Response response = controller.processRequest(request);
         assertNotNull("Must not return a null response", response);
         assertEquals("Response should be type of SampleResponse",
